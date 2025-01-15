3839825fa84a061e85e7ed24d21f23c9a01c69f4
Moritz Wunderwald <moritzwunderwald@yahoo.de>
2025-01-15 21:42:13 +0100

    added gui reflection of input validity

--- ccf/app.py
+++ ccf/app.py
@@ -9,40 +9,31 @@ tk.set_default_color_theme("dark-blue")
 app = tk.CTk()  
 app.geometry("1280x720")
 
+# ---------------------
+# GLOBAL STATE & EVENTS
+# ---------------------
+
+# input validation trackers
+val_CORRELATION_SETTINGS_VALID = tk.BooleanVar(value=True)
+val_WINDOW_SIZE_VALID = tk.BooleanVar(value=True)
+val_STEP_SIZE_VALID = tk.BooleanVar(value=True)
+val_MAX_LAG_VALID = tk.BooleanVar(value=True)
+
+# main event callback
+def PARAMS_CHANGED():
+    print('PARAMS_CHANGED')
+
 # ----------------
 # INPUT VALIDATION
 # ----------------
+
 def on_validate_numeric_input(input):
     if input == "" or input.isdigit():
         return True
     return False
 validate_numeric_input = app.register(on_validate_numeric_input)
 
-# ---------------------------
-# CALLBACKS & STATE VARIABLES
-# ---------------------------
-# main event callback
-def PARAMS_CHANGED():
-    print('PARAMS_CHANGED')
-
-# set up app state variables 
-val_CORRELATION_SETTINGS_VALID = tk.BooleanVar(value=True)
-val_WINDOW_SIZE_VALID = tk.BooleanVar(value=True)
-val_STEP_SIZE_VALID = tk.BooleanVar(value=True)
-val_MAX_LAG_VALID = tk.BooleanVar(value=True)
-val_checkbox_absolute_corr = tk.BooleanVar(value=False)
-val_checkbox_filter_data = tk.BooleanVar(value=False)
-val_checkbox_IBI = tk.BooleanVar(value=True)
-val_checkbox_EDA = tk.BooleanVar(value=False)
-val_checkbox_windowed_xcorr = tk.BooleanVar(value=True)
-val_window_size_input = tk.StringVar(value='10')
-val_step_size_input = tk.StringVar(value='1')
-val_max_lag_input = tk.StringVar(value='5')
-val_window_size = tk.IntVar(value=10)
-val_step_size = tk.IntVar(value=1)
-val_max_lag = tk.IntVar(value=5)
-
-# entry callbacks
+# entry validation
 def check_window_size():
     # window size must be at least 3
     # TODO input should turn red if value is invalid
@@ -70,8 +61,27 @@ def check_correlation_settings():
     max_lag_is_valid = check_max_lag()
     correlation_settings_valid = window_size_is_valid and step_size_is_valid and max_lag_is_valid
     val_CORRELATION_SETTINGS_VALID.set(correlation_settings_valid)
+    PARAMS_CHANGED()
     return correlation_settings_valid
 
+# ---------------------------
+# CALLBACKS & STATE VARIABLES
+# ---------------------------
+
+# set up app state variables 
+val_checkbox_absolute_corr = tk.BooleanVar(value=False)
+val_checkbox_filter_data = tk.BooleanVar(value=False)
+val_checkbox_IBI = tk.BooleanVar(value=True)
+val_checkbox_EDA = tk.BooleanVar(value=False)
+val_checkbox_windowed_xcorr = tk.BooleanVar(value=True)
+val_window_size_input = tk.StringVar(value='10')
+val_step_size_input = tk.StringVar(value='1')
+val_max_lag_input = tk.StringVar(value='5')
+val_window_size = tk.IntVar(value=10)
+val_step_size = tk.IntVar(value=1)
+val_max_lag = tk.IntVar(value=5)
+
+# entry callbacks
 def on_window_size_input_change(name, index, mode):
     new_str_val = val_window_size_input.get()
     if new_str_val == '':
@@ -80,7 +90,6 @@ def on_window_size_input_change(name, index, mode):
         new_val = int(new_str_val)
         val_window_size.set(new_val)
     check_correlation_settings()
-    PARAMS_CHANGED()
 val_window_size_input.trace_add("write", on_window_size_input_change)
 
 def on_step_size_input_change(name, index, mode):
@@ -91,7 +100,6 @@ def on_step_size_input_change(name, index, mode):
         new_val = int(new_str_val)
         val_step_size.set(new_val)
     check_correlation_settings()
-    PARAMS_CHANGED()
 val_step_size_input.trace_add("write", on_step_size_input_change)
 
 def on_max_lag_input_change(name, index, mode):
@@ -102,10 +110,7 @@ def on_max_lag_input_change(name, index, mode):
         new_val = int(new_str_val)
         val_max_lag.set(new_val)
     check_correlation_settings()
-    PARAMS_CHANGED()
 val_max_lag_input.trace_add("write", on_max_lag_input_change)
-
-
 
 # checkbox callbacks
 def on_absolute_corr_change():
@@ -164,15 +169,15 @@ checkbox_is_eda_data = tk.CTkCheckBox(frame_parameter_group, text='Windowed XCor
 checkbox_is_eda_data.grid(row=6, column=0, sticky="w", padx=10, pady=5)
 label_window_size = tk.CTkLabel(frame_parameter_group, text='Window Size')
 label_window_size.grid(row=7, column=0, sticky="w", padx=10, pady=5)
-entry_window_size = tk.CTkEntry(frame_parameter_group, validate="key", validatecommand=(validate_numeric_input, "%P"), textvariable=val_window_size_input)
+entry_window_size = tk.CTkEntry(frame_parameter_group, validate="key", validatecommand=(validate_numeric_input, "%P"), textvariable=val_window_size_input, border_color='#777777')
 entry_window_size.grid(row=7, column=1, sticky="w", padx=10, pady=5)
 label_max_lag = tk.CTkLabel(frame_parameter_group, text='Max Lag')
 label_max_lag.grid(row=8, column=0, sticky="w", padx=10, pady=5)
-entry_max_lag = tk.CTkEntry(frame_parameter_group, validate="key", validatecommand=(validate_numeric_input, "%P"), textvariable=val_max_lag_input)
+entry_max_lag = tk.CTkEntry(frame_parameter_group, validate="key", validatecommand=(validate_numeric_input, "%P"), textvariable=val_max_lag_input, border_color='#777777')
 entry_max_lag.grid(row=8, column=1, sticky="w", padx=10, pady=5)
 label_step_size = tk.CTkLabel(frame_parameter_group, text='Step Size')
 label_step_size.grid(row=9, column=0, sticky="w", padx=10, pady=5)
-entry_step_size = tk.CTkEntry(frame_parameter_group, validate="key", validatecommand=(validate_numeric_input, "%P"), textvariable=val_step_size_input)
+entry_step_size = tk.CTkEntry(frame_parameter_group, validate="key", validatecommand=(validate_numeric_input, "%P"), textvariable=val_step_size_input, border_color='#777777')
 entry_step_size.grid(row=9, column=1, sticky="w", padx=10, pady=5)
 checkbox_absolute_corr = tk.CTkCheckBox(frame_parameter_group, text='Absolute Correlation Values', variable=val_checkbox_absolute_corr, command=on_absolute_corr_change)
 checkbox_absolute_corr.grid(row=10, column=0, sticky="w", padx=10, pady=5)
@@ -180,6 +185,20 @@ checkbox_absolute_corr.grid(row=10, column=0, sticky="w", padx=10, pady=5)
 # export
 button_export = tk.CTkButton(frame_parameter_group, text='Export', command=export_data)
 button_export.grid(row=11, column=0, sticky="w", padx=10, pady=5)
+
+# ---------------------
+# PARAMETER GUI UPDATES
+# ---------------------
+def update_color_window_size_entry(*args):
+    entry_window_size.configure(border_color='#777777' if val_WINDOW_SIZE_VALID.get() else 'red')
+val_WINDOW_SIZE_VALID.trace_add('write', update_color_window_size_entry)
+def update_color_step_size_entry(*args):
+    entry_step_size.configure(border_color='#777777' if val_STEP_SIZE_VALID.get() else 'red')
+val_STEP_SIZE_VALID.trace_add('write', update_color_step_size_entry)
+def update_color_max_lag_entry(*args):
+    entry_max_lag.configure(border_color='#777777' if val_MAX_LAG_VALID.get() else 'red')
+val_MAX_LAG_VALID.trace_add('write', update_color_max_lag_entry)
+
 
 # run app
 app.mainloop()
