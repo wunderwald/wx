6c4930aec4a71911a9882477615b91d941d101db
Moritz Wunderwald <moritzwunderwald@yahoo.de>
2025-09-17 11:40:15 +0200

    finished dyad making

--- preprocessing/group_to_dyads.py
+++ preprocessing/group_to_dyads.py
@@ -3,6 +3,7 @@ import re
 import os
 from pathlib import Path
 from itertools import combinations
+import shutil
 
 # -------
 # HELPERS
@@ -21,6 +22,8 @@ def parse_filename(filename):
         return match.groupdict()
     else:
         return {}
+def find_dict_by_field(dicts, field, value):
+    return next((d for d in dicts if d.get(field) == value), None)
 
 # --------
 # I/O DATA
@@ -74,8 +77,19 @@ dyads = list(combinations([s for s in subjects], 2))
 
 # for each dyad , for each condition, create a subdirectory in the output dir with two xlsx files 
 for (subject_0, subject_1) in dyads:
-    print(subject_0['id'], subject_1['id'])
     conditions_sub_0 = [c['condition'] for c in subject_0['conditions']]
     conditions_sub_1 = [c['condition'] for c in subject_1['conditions']]
     common_conditions = set(conditions_sub_0) & set(conditions_sub_1)
-    print(list(common_conditions))
+    for condition in common_conditions:
+        # get xlsx files for current condition
+        subject_0_xlsx_path = find_dict_by_field(subject_0['conditions'], 'condition', condition)['path']
+        subject_1_xlsx_path = find_dict_by_field(subject_1['conditions'], 'condition', condition)['path']
+
+        # make output subdir
+        output_subdir = f"dyad_{subject_0['id']}_{subject_1['id']}_{condition}"
+        output_subdir_path = Path.joinpath(output_dir_abs, output_subdir)
+        output_subdir_path.mkdir(parents=True, exist_ok=True)
+        dest_0 = output_subdir_path / Path(subject_0_xlsx_path).name
+        dest_1 = output_subdir_path / Path(subject_1_xlsx_path).name
+        shutil.copy(subject_0_xlsx_path, dest_0)
+        shutil.copy(subject_1_xlsx_path, dest_1)
